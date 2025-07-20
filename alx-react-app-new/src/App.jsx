2612043525fa01e37c888d3e24887d1bc472d18c
FaizaSuraw <itssumeya@gmail.com>
2025-07-20 11:22:36 +0300

    Added Counter.jsx file

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,20 +1,19 @@
+import React from 'react';
 import Header from './components/Header';
+import UserProfile from './components/UserProfile';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
-import UserProfile from './components/UserProfile';
+import Counter from './components/Counter'; 
 
 function App() {
   return (
-    <>
+    <div>
       <Header />
       <MainContent />
-      <UserProfile
-        name="Alice"
-        age="25"
-        bio="Loves hiking and photography"
-      />
+      <UserProfile name="Faiza" age={22} bio="React learner and full-stack explorer!" />
+      <Counter /> 
       <Footer />
-    </>
+    </div>
   );
 }
 
